e958d30901d8f6b840961cabf84965ef9ede286e
Robin Law <robin.law@metaltoad.com>
2018-06-12 17:18:15 -0700

    add super hacky outage tracker
    State management is garbage. Will refactor soon

--- index.js
+++ index.js
@@ -61,31 +61,67 @@ const environments = [
     view: blessed.text(getBaseBox(`${eastBanner}`))
   }
 ]
-const state = { environments: environments };
+const outageBox = blessed.text({
+  parent: layout,
+  width: '25%',
+  height: '50%',
+  label: "Outages",
+  content: "",
+  border: {
+    type: 'line'
+  },
+});
+const state = { environments: environments, outages: [] };
 state.environments.slice(0, 2).forEach(env => layout.append(env.view));
+layout.append(outageBox);
 screen.render();
 
 const updateEnvironmentStatuses = () =>
   state.environments.forEach(env => {
     axios.get(env.url)
       .then(response => {
+        if (env.healthy === false) {
+          // TODO: State is getting messy! Clean it up!
+          resolveOutage(env.name);
+        }
         env.healthy = true;
         env.view.setContent(`${env.banner}`);
         env.view.style = { ...env.view.style, bg: goodColor };
-        env.view.setLabel(`Last Checked: ${moment().toLocaleString()}`);
+        env.view.setLabel(`Last Checked: ${moment().toLocaleString()} - Result: ${response.status}`);
         screen.render();
       })
       .catch(error => {
-        env.lastUnhealthy = env.healthy || env.healthy === undefined ?
-          moment() : env.lastUnhealthy;
+        if (env.healthy || env.healthy === undefined) {
+          // TODO: State is getting messy! Clean it up!
+          env.lastUnhealthy = moment();
+          addOutage(env.name);
+        }
         env.healthy = false;
         const lastUnhealthyDisplay = getLastUnhealthyBanner(env.lastUnhealthy);
         env.view.setContent(`${env.banner}\n\n${lastUnhealthyDisplay}`);
         env.view.style = { ...env.view.style, bg: badColor };
-        env.view.setLabel(`Last Checked: ${moment().toLocaleString()}`);
+        env.view.setLabel(`Last Checked: ${moment().toLocaleString()} - Result: ${error}`);
         screen.render();
       });
   });
+
+const addOutage = (envName) => {
+  // TODO: State is getting messy! Clean it up!
+  state.outages.push({ env: envName, start: moment() });
+  updateOutageView();
+}
+
+const resolveOutage = (envName) => {
+  // TODO: State is getting messy! Clean it up!
+  var outagesForEnv = (state.outages || [{}]).filter(outage => outage.env === envName);
+  (outagesForEnv[state.outages.length-1] || {}).end = moment();
+  updateOutageView();
+}
+
+const updateOutageView = () => {
+  outageBox.setContent([...state.outages].reverse().map(outage => `${outage.env} - ${outage.start.format("LLL")} - ${outage.end ? outage.end.format("LLL") : "Ongoing"}`).join("\n"));
+  screen.render();
+}
 
 updateEnvironmentStatuses();
 setInterval(updateEnvironmentStatuses, 300000);
