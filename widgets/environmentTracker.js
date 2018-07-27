798ee2566f15eb674aaa222b65329dc32b8cf33e
Robin Law <robin.law@metaltoad.com>
2018-07-27 11:39:54 -0700

    extract envTracker sqlite adapter

--- widgets/environmentTracker.js
+++ widgets/environmentTracker.js
@@ -2,62 +2,7 @@ const blessed = require('blessed');
 const figlet = require('figlet');
 const moment = require('moment');
 const uuid = require("uuid").v4;
-
-const sqliteDateTimeFormat = 'YYYY-MM-DD HH:mm:ss';
-const createTable = (db) => db.run(
-  `CREATE TABLE IF NOT EXISTS Outages 
-  (
-    id VARCHAR(36) PRIMARY KEY,
-    environment VARCHAR(50),
-    outageBegan DATETIME,
-    outageEnded DATETIME,
-    reason VARCHAR(255)
-  )`
-);
-
-const persistOutage = (db, outage) => {
-  db.run(
-    `INSERT INTO Outages (id, environment, outageBegan, reason)
-     VALUES (
-      '${outage.id}',
-      '${outage.environment}',
-      '${outage.outageBegan.format(sqliteDateTimeFormat)}',
-      '${outage.reason}'
-   )`);
-};
-
-const persistResolveOutage = (db, outage) => {
-  db.run(
-    `UPDATE Outages
-     SET outageEnded='${outage.outageEnded.format(sqliteDateTimeFormat)}'
-     WHERE id='${outage.id}'`
-  );
-};
-
-const outageFromDbRow = (outageRow) => ({
-  id: outageRow.id,
-  environment: outageRow.environment,
-  outageBegan: moment(outageRow.outageBegan),
-  outageEnded: outageRow.outageEnded ? moment(outageRow.outageEnded) : undefined,
-  reason: outageRow.reason
-});
-
-const loadOutages = (instance) => {
-  instance.db.all(`
-  SELECT *
-  FROM Outages
-  WHERE environment = '${instance.name}'
-  ORDER BY outageBegan DESC
-  LIMIT 10`, (err, rows) => {
-    if (err) { console.log(err); return; };
-    rows.forEach(row => instance.outages.unshift(outageFromDbRow(row)));
-
-    // TODO: Race condition: If the first state check returns before the outages load, the currentOutage will fall out of sync
-    if (rows[0] && !rows[0].outageEnded && !instance.currentOutage) {
-      instance.currentOutageId = rows[0].id;
-    }
-  });
-}
+const adapterFactory = require("../adapters/environmentTrackerSqliteAdapter");
 
 const goodColor = 'green';
 const maybeColor = 'grey';
@@ -108,7 +53,7 @@ const createOutage = (instance, reason) => {
     reason
   }
   instance.currentOutageId = newOutage.id;
-  persistOutage(instance.db, newOutage);
+  instance.sqlAdapter.persistOutage(newOutage);
   instance.outages.push(newOutage);
 };
 
@@ -117,7 +62,7 @@ const getCurrentOutage = (instance) => instance.outages.find(outage => outage.id
 const markOutageResolved = (instance) => {
   const currentOutage = getCurrentOutage(instance)
   currentOutage.outageEnded = moment();
-  persistResolveOutage(instance.db, currentOutage);
+  instance.sqlAdapter.persistResolveOutage(currentOutage);
   delete instance.currentOutageId;
 }
 
@@ -170,7 +115,8 @@ const updateOutageView = (instance) => {
 }
 
 const environmentTrackerFactory = (axios, screen, db, name, url, viewWidthPercent, viewHeightPercent) => {
-  createTable(db);
+  const sqlAdapter = adapterFactory(db);
+  sqlAdapter.createTable();
   const currentStatusViewWeight = .67;
   const currentStatusViewDimensions = calculateDimensions(viewWidthPercent, viewHeightPercent, currentStatusViewWeight);
   const outagesViewDimensions = calculateDimensions(viewWidthPercent, viewHeightPercent, 1 - currentStatusViewWeight);
@@ -183,11 +129,12 @@ const environmentTrackerFactory = (axios, screen, db, name, url, viewWidthPercen
     outages: [],
     banner: figlet.textSync(name, figletStyleBanner),
     currentStatusView: createCurrentStatusView(currentStatusViewDimensions, name),
-    outagesView: createOutagesView(outagesViewDimensions, name)
+    outagesView: createOutagesView(outagesViewDimensions, name),
+    sqlAdapter: sqlAdapter
   };
 
   // TODO: Race condition: If the first state check returns before the outages load, the currentOutage will fall out of sync
-  loadOutages(instance);
+  sqlAdapter.loadOutages(instance);
 
   const views = [ instance.currentStatusView, instance.outagesView ];
 
